5d24ce4122f620d438d872cafe5c3f83183096df
Upbet <registermain@gmail.com>
2015-11-17 08:59:20 +0100

    Cronometro
    Cronometro en C

---
+++ cronometro.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <windows.h>
+
+main(){
+	
+	int h,min,seg,x=1000;
+	
+	for(h = 0; h < 24; h++){
+		
+		for(min = 0; min < 60; min++){
+			
+			for(seg = 0; seg < 60; seg++){
+				
+				printf("%02i:%02i:%02i\r",h,min,seg);
+				Sleep(x);
+			}
+		}
+		
+	}
+	
+
+	
+	return 0;
+	getchar();
+}
